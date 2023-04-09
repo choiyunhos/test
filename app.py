ec8cb711a30ded4a5141fcb188f53d61fb81deab
choiyunho <chy2803@sillain.ac.kr>
2023-04-09 14:15:31 +0900

    2

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 #app.py
-
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -17,48 +16,48 @@ class Board(db.Model):
     def __repr__(self):
         return '<Board %r>' % self.id
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    if request.method == 'POST':
-        board_title = request.form['title']
-        board_content = request.form['content']
-        new_board = Board(title=board_title, content=board_content)
+    @app.route('/', methods=['POST', 'GET'])
+    def index():
+        if request.method == 'POST':
+            board_title = request.form['title']
+            board_content = request.form['content']
+            new_board = Board(title=board_title, content=board_content)
+
+            try:
+                db.session.add(new_board)
+                db.session.commit()
+                return redirect('/')
+            except:
+                return 'There was an issue adding your board'
+
+        else:
+            boards = Board.query.order_by(Board.date_created).all()
+            return render_template('index.html', boards=boards)
+
+    @app.route('/delete/<int:id>')
+    def delete(id):
+        board_to_delete = Board.query.get_or_404(id)
 
         try:
-            db.session.add(new_board)
+            db.session.delete(board_to_delete)
             db.session.commit()
             return redirect('/')
         except:
-            return 'There was an issue adding your board'
+            return 'There was a problem deleting that board'
+        
+    @app.route('/update/<int:id>', methods=['GET', 'POST'])
+    def update(id):
+        board = Board.query.get_or_404(id)
 
-    else:
-        boards = Board.query.order_by(Board.date_created).all()
-        return render_template('index.html', boards=boards)
+        if request.method == 'POST':
+            board.title = request.form['title']
+            board.content = request.form['content']
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    board_to_delete = Board.query.get_or_404(id)
-
-    try:
-        db.session.delete(board_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting that board'
-    
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    board = Board.query.get_or_404(id)
-
-    if request.method == 'POST':
-        board.title = request.form['title']
-        board.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your board'
+            try:
+                db.session.commit()
+                return redirect('/')
+            except:
+                return 'There was an issue updating your board'
 
 if __name__ == "__main__":
     app.run(debug=True)
